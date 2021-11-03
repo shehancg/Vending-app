f80f26323caf780fd917dd563a73894ac31fe8b7
shehancg <shehanchamik@gmail.com>
2021-11-04 03:03:23 +0530

    commit3
    COMPLETED MENU PANEL

---
+++ app/src/main/java/com/example/vendingapp/MenuScreen.java
@@ -0,0 +1,10 @@
+package com.example.vendingapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class MenuScreen {
+}
